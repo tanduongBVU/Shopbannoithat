9f5f6404b0afe4e5ac4d57e7ac9604847896409c
Nguyen Tan Duong <duongnguyentan800@gmail.com>
2026-03-13 18:52:35 +0700

    làm giao diện 1

---
+++ Shopbannoithat/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Shopbannoithat.Models;
+
+namespace Shopbannoithat.Data
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Bảng của website
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ Shopbannoithat/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace Shopbannoithat.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public string Status { get; set; }
+    }
+}
