649b23b097e29786c75579eab32bfb6e27816318
Nguyen Tan Duong <duongnguyentan800@gmail.com>
2026-03-14 13:01:03 +0700

    thêm giỏ hàng (theo từng tài khoản)

--- Shopbannoithat/Data/ApplicationDbContext.cs
+++ Shopbannoithat/Data/ApplicationDbContext.cs
@@ -19,5 +19,6 @@ namespace Shopbannoithat.Data
         public DbSet<Order> Orders { get; set; }
 
         public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Cart> Carts { get; set; }
     }
 }
---
+++ Shopbannoithat/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿namespace Shopbannoithat.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
